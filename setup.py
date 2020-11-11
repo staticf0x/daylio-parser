b56f727d0770b145ea513e06bcab0a03345084c0
Meesha <44530786+meesha7@users.noreply.github.com>
2020-11-11 14:49:21 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here/'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='daylio-parser',
+    version='0.0.1',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/meesha7/daylio-parser',
+    author='Meesha',
+    license='MIT',
+    classifiers=[
+    ],
+    keywords='',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.6, <4',
+    install_requires=[],
+    extras_require={
+        'dev': ['pycodestyle', 'isort'],
+        'test': ['green']
+    }
+)
