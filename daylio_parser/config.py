748e6ce54a8e2e1e703579a369bcfac3ddb6214f
Meesha <44530786+meesha7@users.noreply.github.com>
2021-09-12 12:11:00 +0200

    #4 Rework custom moods

--- daylio_parser/config.py
+++ daylio_parser/config.py
@@ -4,11 +4,19 @@ from dataclasses import dataclass
 from typing import List, Tuple
 
 DEFAULT_MOODS = [
-    ('awful', '#6C7679'),
-    ('bad', '#5579A7'),
-    ('meh', '#9454A3'),
-    ('good', '#4CA369'),
-    ('rad', '#FF8500'),
+    (1, 'awful'),
+    (2, 'bad'),
+    (3, 'meh'),
+    (4, 'good'),
+    (5, 'rad'),
+]
+
+DEFAULT_COLOR_PALETTE = [
+    '#6C7679',
+    '#5579A7',
+    '#9454A3',
+    '#4CA369',
+    '#FF8500',
 ]
 
 
@@ -23,14 +31,10 @@ class Mood:
 
 
 class MoodConfig:
-    """
-    Configure mood levels and their properties.
+    """Configure mood levels and their properties."""
 
-    TODO: Add color generator when there's too many moods
-    """
-
-    def __init__(self, mood_list: List[Tuple[str, str]] = None):
-        """Create the config with a list of moods: [(name, color), ...].
+    def __init__(self, mood_list: List[Tuple[int, str]] = None, color_palette: List[str] = None):
+        """Create the config with a list of moods: [(level, name), ...].
 
         If no moods are provided, then the config is created
         with a default set of 5 moods.
@@ -38,39 +42,73 @@ class MoodConfig:
         self.moods: List[Mood] = []
         self.__map = {}
 
-        if mood_list:
-            self.__load_moods(mood_list)
-        else:
-            self.__load_moods(DEFAULT_MOODS)
+        if not mood_list:
+            mood_list = DEFAULT_MOODS
 
-    def from_list(self, mood_list: List[Tuple[str, str]]):
-        """Update the config with a list of moods: [(name, color), ...]."""
-        self.__load_moods(mood_list)
+        if not color_palette:
+            color_palette = DEFAULT_COLOR_PALETTE
+
+        self.__load_moods(mood_list, color_palette)
+
+    def from_list(self, mood_list: List[Tuple[int, str]], color_palette: List[str] = None):
+        """
+        Update the config with a list of moods: [(level, name), ...].
+
+        Optionally pass also a color_palette, if not provided, the default palette is used.
+        """
+        if not color_palette:
+            color_palette = DEFAULT_COLOR_PALETTE
+
+        self.__load_moods(mood_list, color_palette)
 
     def get(self, mood_name: str) -> Mood:
         """Return a Mood by its name."""
         return self.__map[mood_name]
 
-    def __load_moods(self, mood_list: List[Tuple[str, str]]):
+    def __load_moods(self, mood_list: List[Tuple[int, str]], color_palette: List[str] = None):
+        self.__validate_mood_list(mood_list)
+        self.__validate_color_palette(color_palette)
+
         self.moods = []
         self.__map = {}
 
-        for level, (name, color) in enumerate(mood_list):
-            if level == 0:
+        for level, name in mood_list:
+            if level == 1:
                 # First (worst) mood
                 b_lower = 1
                 b_upper = 1.5
-            elif level == len(mood_list) - 1:
+            elif level == 5:
                 # Last (best) mood
-                b_lower = level + 0.5
-                b_upper = level + 1.01
+                b_lower = 4.5
+                b_upper = 5.01
             else:
-                b_lower = level + 0.5
-                b_upper = level + 1.5
+                b_lower = level - 1 + 0.5
+                b_upper = level - 1 + 1.5
 
             boundaries = (b_lower, b_upper)
 
-            mood = Mood(name, level + 1, color, boundaries)
+            mood = Mood(name, level, color_palette[level - 1], boundaries)
 
             self.moods.append(mood)
             self.__map[name] = mood
+
+    def __validate_mood_list(self, mood_list: List[Tuple[int, str, str]]):
+        """Validate the provided mood list."""
+        for mood in mood_list:
+            if not len(mood) == 2:
+                raise ValueError('Moods have to be (level, name)')
+
+            level = mood[0]
+
+            if level not in range(1, 6):
+                raise ValueError(f'Mood level {level} is not valid')
+
+        all_levels = set([mood[0] for mood in mood_list])
+
+        if all_levels != set(range(1, 6)):
+            raise ValueError('There has to be at least one mood for each level of {1..6}')
+
+    def __validate_color_palette(self, color_palette: List[str] = None):
+        """Validate the provided color palette."""
+        if not len(color_palette) == 5:
+            raise ValueError('Color palette must contain exactly 5 colors')
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from daylio_parser.config import Mood, MoodConfig
+from daylio_parser.config import DEFAULT_COLOR_PALETTE, DEFAULT_MOODS, Mood, MoodConfig
 
 
 class TestConfig(TestCase):
@@ -14,30 +14,37 @@ class TestConfig(TestCase):
         self.assertEqual(m.moods[0].name, 'awful')
         self.assertEqual(m.moods[0].level, 1)
         self.assertEqual(m.moods[0].boundaries, (1, 1.5))
+        self.assertEqual(m.moods[0].color, DEFAULT_COLOR_PALETTE[0])
 
         self.assertEqual(m.moods[1].name, 'bad')
         self.assertEqual(m.moods[1].level, 2)
         self.assertEqual(m.moods[1].boundaries, (1.5, 2.5))
+        self.assertEqual(m.moods[1].color, DEFAULT_COLOR_PALETTE[1])
 
         self.assertEqual(m.moods[2].name, 'meh')
         self.assertEqual(m.moods[2].level, 3)
         self.assertEqual(m.moods[2].boundaries, (2.5, 3.5))
+        self.assertEqual(m.moods[2].color, DEFAULT_COLOR_PALETTE[2])
 
         self.assertEqual(m.moods[3].name, 'good')
         self.assertEqual(m.moods[3].level, 4)
         self.assertEqual(m.moods[3].boundaries, (3.5, 4.5))
+        self.assertEqual(m.moods[3].color, DEFAULT_COLOR_PALETTE[3])
 
         self.assertEqual(m.moods[4].name, 'rad')
         self.assertEqual(m.moods[4].level, 5)
         self.assertEqual(m.moods[4].boundaries, (4.5, 5.01))
+        self.assertEqual(m.moods[4].color, DEFAULT_COLOR_PALETTE[4])
 
     def test_custom_moods(self):
         """Here we test that custom moods have correctly computed boundaries."""
 
         moods = [
-            ('bad', 'red'),
-            ('neutral', 'orange'),
-            ('good', 'green'),
+            (1, 'bad'),
+            (2, 'almost bad'),
+            (3, 'neutral'),
+            (4, 'almost good'),
+            (5, 'good'),
         ]
 
         m = MoodConfig(moods)
@@ -46,13 +53,96 @@ class TestConfig(TestCase):
         self.assertEqual(m.moods[0].level, 1)
         self.assertEqual(m.moods[0].boundaries, (1, 1.5))
 
-        self.assertEqual(m.moods[1].name, 'neutral')
+        self.assertEqual(m.moods[1].name, 'almost bad')
         self.assertEqual(m.moods[1].level, 2)
         self.assertEqual(m.moods[1].boundaries, (1.5, 2.5))
 
-        self.assertEqual(m.moods[2].name, 'good')
+        self.assertEqual(m.moods[2].name, 'neutral')
         self.assertEqual(m.moods[2].level, 3)
-        self.assertEqual(m.moods[2].boundaries, (2.5, 3.01))
+        self.assertEqual(m.moods[2].boundaries, (2.5, 3.5))
+
+        self.assertEqual(m.moods[3].name, 'almost good')
+        self.assertEqual(m.moods[3].level, 4)
+        self.assertEqual(m.moods[3].boundaries, (3.5, 4.5))
+
+        self.assertEqual(m.moods[4].name, 'good')
+        self.assertEqual(m.moods[4].level, 5)
+        self.assertEqual(m.moods[4].boundaries, (4.5, 5.01))
+
+    def test_validation(self):
+        """Test wrong mood lists."""
+
+        # Only 1 mood
+        moods = [
+            (1, 'bad', 'red'),
+        ]
+
+        with self.assertRaises(ValueError):
+            MoodConfig(moods)
+
+        # Bad formatting
+        moods = [
+            ('bad',),
+            ('almost bad',),
+            ('neutral',),
+            ('almost good',),
+            ('good',),
+        ]
+
+        with self.assertRaises(ValueError):
+            MoodConfig(moods)
+
+        # Missing level
+        moods = [
+            (1, 'bad'),
+            (2, 'almost bad'),
+            (3, 'neutral'),
+            (4, 'almost good'),
+            (4, 'good'),
+        ]
+
+        with self.assertRaises(ValueError):
+            MoodConfig(moods)
+
+        # Wrong level
+        moods = [
+            (1, 'bad'),
+            (2, 'almost bad'),
+            (3, 'neutral'),
+            (4, 'almost good'),
+            (5, 'good'),
+            (6, 'rad'),
+        ]
+
+        with self.assertRaises(ValueError):
+            MoodConfig(moods)
+
+        # Wrong palette
+        colors = [
+            'red',
+        ]
+
+        with self.assertRaises(ValueError):
+            MoodConfig(DEFAULT_MOODS, colors)
+
+    def test_custom_colors(self):
+        """Test custom color palette."""
+
+        colors = [
+            'black',
+            'red',
+            'orange',
+            'yellow',
+            'green',
+        ]
+
+        m = MoodConfig(DEFAULT_MOODS, colors)
+
+        self.assertEqual(m.moods[0].color, 'black')
+        self.assertEqual(m.moods[1].color, 'red')
+        self.assertEqual(m.moods[2].color, 'orange')
+        self.assertEqual(m.moods[3].color, 'yellow')
+        self.assertEqual(m.moods[4].color, 'green')
 
     def test_get_mood(self):
         """Test getter by mood name."""
